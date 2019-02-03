6423cb18f19d19b2640782317026db5b9670f840
Yaacob Noor <maxcobmara@gmail.com>
2019-02-03 20:21:03 +0800

    static pages

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -4,11 +4,13 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get root_url
     assert_response :success
+    assert_select "title", "Info Harga Minyak"
   end
 
   test "should get about" do
     get pages_about_url
     assert_response :success
+    assert_select "title", "About Harga Minyak"
   end
 
 end
