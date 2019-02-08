b60fb0ed5b13f7682e63b21f38b5da5c46240162
Yaacob Noor <yaacob@revasia.com>
2019-02-08 14:18:37 +0800

    home layout on fleek

--- app/models/price.rb
+++ app/models/price.rb
@@ -3,8 +3,11 @@ class Price < ApplicationRecord
   belongs_to :entity
   before_save :set_entity_name
 
+  default_scope {order(price_on: :desc)}
 
   def set_entity_name
     self.entity_name = Entity.find(entity_id).name
   end
+
+
 end
