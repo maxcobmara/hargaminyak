942ff7265177e44c62a64335c96c49bacf1f813d
Yaacob Noor <yaacob@revasia.com>
2019-02-18 18:12:54 +0800

    ui updates for data entry

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,18 @@
 module PagesHelper
+
+  def new_icon
+    "<i class='icon icon-plus'></i>".html_safe
+  end
+
+  def show_icon
+    "<i class='icon icon-more-horiz'></i> &nbsp;".html_safe
+  end
+
+  def edit_icon
+    "<i class='icon icon-edit'></i> &nbsp;".html_safe
+  end
+
+  def trash_icon
+    "<i class='icon icon-delete'></i> &nbsp;".html_safe
+  end
 end
