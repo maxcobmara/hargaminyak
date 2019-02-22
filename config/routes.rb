47e65dc847f56d0ee68e32b2895ea0e7aa9d2162
Yaacob Noor <yaacob@revasia.com>
2019-02-22 16:35:20 +0800

    fix router

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
 
-  get '*path' => redirect('/') unless Rails.env.development?
+
 
   resources :entities
   resources :prices
   get 'pages/about'
   root 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get '*path' => redirect('/') unless Rails.env.development?
 end
