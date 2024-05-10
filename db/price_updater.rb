24f35e40078bbfeb2fe1076eb5b76b83eae9fafa
Yaacob Noor <maxcobmara@gmail.com>
2024-05-10 16:41:20 +0800

    seed fixes

--- db/price_updater.rb
+++ db/price_updater.rb
@@ -1,4 +1,15 @@
 Price.create!([
+  {entity_id: 1, entity_name: "RON95",  price_on: "09 May 2024".to_date, price:	205},
+  {entity_id: 2, entity_name: "RON97",  price_on: "09 May 2024".to_date, price:	347}, 
+  {entity_id: 3, entity_name: "Diesel", price_on: "09 May 2024".to_date, price:	215},
+  {entity_id: 4, entity_name: "Diesel Euro5", price_on: "09 May 2024".to_date, price:	235},  
+  {entity_id: 5, entity_name: "RON100", price_on: "09 May 2024".to_date, price:	500}, 
+  {entity_id: 6, entity_name: "VPower", price_on: "09 May 2024".to_date, price:	640}
+])
+
+
+
+Price.create!([
   {entity_id: 1, entity_name: "RON95",  price_on: "25 Apr 2024".to_date, price:	205},
   {entity_id: 2, entity_name: "RON97",  price_on: "25 Apr 2024".to_date, price:	347}, 
   {entity_id: 3, entity_name: "Diesel", price_on: "25 Apr 2024".to_date, price:	215},
@@ -12,5 +23,19 @@ Price.create!([
 ])
   
   
-https://www.petron.com.my/what-we-offer/products/fuels/retail-fuels/petron-blaze-100-euro-4m/  
-  
+# https://www.petron.com.my/what-we-offer/products/fuels/retail-fuels/petron-blaze-100-euro-4m/  
+# https://www.shell.com.my/motorists/shell-fuels/fuelprice.html  
+
+
+puts "2024 Q2 Fuel Prices"
+Price.create!([
+  {entity_id: 4, entity_name: "Diesel Euro5", price_on: "09 May 2024".to_date, price:	235},  
+  {entity_id: 5, entity_name: "RON100", price_on: "09 May 2024".to_date, price:	500}, 
+  {entity_id: 6, entity_name: "VPower", price_on: "09 May 2024".to_date, price:	640},
+  {entity_id: 4, entity_name: "Diesel Euro5", price_on: "25 Apr 2024".to_date, price:	235},  
+  {entity_id: 5, entity_name: "RON100", price_on: "25 Apr 2024".to_date, price:	500}, 
+  {entity_id: 6, entity_name: "VPower", price_on: "25 Apr 2024".to_date, price:	620},
+  {entity_id: 4, entity_name: "Diesel Euro5", price_on: "25 Apr 2024".to_date, price:	235},  
+  {entity_id: 4, entity_name: "Diesel Euro5", price_on: "25 Apr 2024".to_date, price:	235},  
+  {entity_id: 4, entity_name: "Diesel Euro5", price_on: "25 Apr 2024".to_date, price:	235}
+])
