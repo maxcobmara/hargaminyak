5b787e77ed084178e06aad61d21f874d4a99c416
Yaacob Noor <maxcobmara@gmail.com>
2024-05-17 14:52:14 +0800

    comment puma prod for hatchbox

--- config/puma/production.rb
+++ config/puma/production.rb
@@ -1,20 +1,20 @@
-require 'puma/daemon'
+#require 'puma/daemon'
 threads_count = ENV.fetch("RAILS_MAX_THREADS") { 5 }
 threads threads_count, threads_count
 
-port        ENV.fetch("PORT") { 3000 }
+#port        ENV.fetch("PORT") { 3000 }
 environment "production"
 
-bind  "unix:///var/www/hargaminyak.info/shared/tmp/sockets/puma.sock"
-pidfile "/var/www/hargaminyak.info/shared/tmp/pids/puma.pid"
-state_path "/var/www/hargaminyak.info/shared/tmp/sockets/puma.state"
-directory "/var/www/hargaminyak.info/current"
+#bind  "unix:///var/www/hargaminyak.info/shared/tmp/sockets/puma.sock"
+#pidfile "/var/www/hargaminyak.info/shared/tmp/pids/puma.pid"
+#state_path "/var/www/hargaminyak.info/shared/tmp/sockets/puma.state"
+#directory "/var/www/hargaminyak.info/current"
 
-workers 2
-threads 1,2
+#workers 2
+#threads 1,2
 
-daemonize true
+#daemonize true
 
-activate_control_app 'unix:///var/www/hargaminyak.info/shared/tmp/sockets/pumactl.sock'
-prune_bundler
+#activate_control_app 'unix:///var/www/hargaminyak.info/shared/tmp/sockets/pumactl.sock'
+#prune_bundler
 #plugin :tmp_restart
